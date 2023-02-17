98b3c0d2c1c7aba6b6764dcaccca93ee8212c064
iGameCreep <gamecreepbs@gmail.com>
2023-02-17 18:11:30 +0100

    Little change.

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -6,7 +6,7 @@ const event: BotEvent = {
     name: Events.InteractionCreate,
     execute(client: Client, interaction: ChatInputCommandInteraction) {
         if (interaction.type === InteractionType.ApplicationCommand) {
-            const command: Command = Commands.find(c => c.name === interaction.commandName);
+            const command: Command = Commands.find((c: Command) => c.name === interaction.commandName);
 
             if (!command) return interaction.reply({ content: `❌| Une erreur est survenue ${interaction.member}... Réessaie plus tard...`, ephemeral: true });
     
